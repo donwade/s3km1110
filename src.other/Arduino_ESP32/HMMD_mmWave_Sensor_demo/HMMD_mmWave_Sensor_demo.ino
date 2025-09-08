2bef36c7aa2984e4a23b95a192f7d4a52ac2d3fb
Don Wade <don_wade@rogers.com>
2025-09-08 19:49:59 -0400

    dumping stream

--- src.other/Arduino_ESP32/HMMD_mmWave_Sensor_demo/HMMD_mmWave_Sensor_demo.ino
+++ src.other/Arduino_ESP32/HMMD_mmWave_Sensor_demo/HMMD_mmWave_Sensor_demo.ino
@@ -1,24 +1,36 @@
-#include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(5, 4); // RX, TX
+#define RX2 18
+#define TX2 19
 
-void setup() {
-  // Start the serial communication with a baud rate of 115200
-  Serial.begin(115200);
-  mySerial.begin(115200);
+#include <HardwareSerial.h>
+HardwareSerial mySerial(2);
 
-  // Wait for the serial port to initialize
-  while (!Serial) {
-    delay(100);
-  }
+//#include <SoftwareSerial.h>
+//SoftwareSerial mySerial(5, 4); // RX, TX
 
-  // Hex string to send
-  String hex_to_send = "FDFCFBFA0800120000006400000004030201";
-  sendHexData(hex_to_send);
+String hex_to_send = "FDFCFBFA0800120000006400000004030201";
+
+void setup() 
+{
+
+	// Start the serial communication with a baud rate of 115200
+	Serial.begin(115200);
+
+	mySerial.setPins(18,19);
+	mySerial.begin(115200);
+
+	// Wait for the serial port to initialize
+	while (!Serial) {
+	delay(100);
+	}
+
+	sendHexData(hex_to_send);  // start reporting?
+	// Hex string to send
 }
 
-void loop() {
-  // Read and print serial data
+void loop() 
+{
+ // Read and print serial data
   readSerialData();
 }
 
@@ -34,10 +46,36 @@ void sendHexData(String hexString) {
   mySerial.write(hexBytes, sizeof(hexBytes));
 }
 
-void readSerialData() {
-  // Read and print data from software serial
-  while (mySerial.available() > 0) {
-    char incomingByte = mySerial.read();
-    Serial.print(incomingByte);
-  }
+void readSerialData()
+{
+    static int ctr;
+    char keeper[15];
+
+    int limit = sizeof(keeper);
+
+    // Read and print data from software serial
+    if  (mySerial.available() > 0) 
+    {
+        char incomingByte = mySerial.read();
+        keeper[ctr++] = incomingByte;
+
+        if (ctr == limit)
+        {
+            Serial.println();
+
+            for (ctr = 0; ctr < limit; ctr++)
+            {
+                Serial.printf("%02x ", keeper[ctr]);
+            }
+ 
+            for (ctr = 0; ctr < limit; ctr++)
+            {
+                Serial.printf("%c", (keeper[ctr] > 0x19 && keeper[ctr] < 128) ? 
+                    keeper[ctr] : '.') ;
+            }
+            ctr = 0;
+        }
+        //Serial.print(incomingByte);
+    }
 }
+
